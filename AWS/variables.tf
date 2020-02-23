8eba647eb76c7f8d70943baf13301a774592e936
User <user@Users-iMac.local>
2020-02-23 23:37:17 +0000

    compute2

--- AWS/variables.tf
+++ AWS/variables.tf
@@ -5,9 +5,16 @@ variable "aws_region" {}
 variable "project_name" {}
 
 #-------networking variables
-variable "vpc_cidr" {}
-variable "public_cidrs" {
+variable vpc_cidr {}
+variable public_cidrs {
   type = list
 }
-
 variable "accessip" {}
+
+#-------compute variables
+variable "key_name" {}
+variable "public_key_path" {}
+variable "server_instance_type" {}
+variable "instance_count" {
+  default = 1
+}
